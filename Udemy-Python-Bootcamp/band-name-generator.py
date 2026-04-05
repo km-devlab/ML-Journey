b202bf83e3964be499d00ef3d47ebba94ae4f4f2
km12 <125912478+km-devlab@users.noreply.github.com>
2026-04-05 23:43:52 +0530

    Update band-name-generator.py

--- Udemy-Python-Bootcamp/band-name-generator.py
+++ Udemy-Python-Bootcamp/band-name-generator.py
@@ -1,1 +1,11 @@
+print("Lets Apply What we Learnt!")
+city=input("Whats your city name?\n")
+pet=input("Whats your pet name?\n")
+print("Your Band Name :"+ city+" "+pet)
 
+#OUTPUT
+# Whats your city name?
+# Tokyo
+# Whats your pet name?
+# sunshine
+# Your Band Name :Tokyo sunshine
