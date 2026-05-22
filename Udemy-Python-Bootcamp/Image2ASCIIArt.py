be94f94ab6b3c79c631f35dcc61a711122add205
km12 <125912478+km-devlab@users.noreply.github.com>
2026-05-22 23:58:48 +0530

    Update Image2ASCIIArt.py

--- Udemy-Python-Bootcamp/Image2ASCIIArt.py
+++ Udemy-Python-Bootcamp/Image2ASCIIArt.py
@@ -1,1 +1,35 @@
+from PIL import Image
 
+ASCII_CHARS = "@%#*+=-:. "
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio)
+    return image.resize((new_width, new_height))
+
+def grayify(image):
+    return image.convert("L")
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    return "".join(ASCII_CHARS[pixel // 25] for pixel in pixels)
+
+def main():
+    path = input("Enter image path: ")
+    image = Image.open(path)
+
+    image = resize_image(image)
+    image = grayify(image)
+
+    ascii_str = pixels_to_ascii(image)
+
+    pixel_count = len(ascii_str)
+    ascii_img = "\n".join(ascii_str[i:i+100] for i in range(0, pixel_count, 100))
+
+    print(ascii_img)
+
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_img)
+
+main()
