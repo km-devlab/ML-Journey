95fe9b1f67433156130ba20713d68d7820cb43dd
km12 <125912478+km-devlab@users.noreply.github.com>
2026-05-31 23:58:37 +0530

    Update TextToQRCode.py

--- Udemy-Python-Bootcamp/TextToQRCode.py
+++ Udemy-Python-Bootcamp/TextToQRCode.py
@@ -1,1 +1,24 @@
+# Step 1: Install the library
 
+# Open Command Prompt and run:
+
+# pip install qrcode[pil]
+
+import qrcode
+
+data = input("Enter text or URL: ")
+
+img = qrcode.make(data)
+
+filename = input("Enter file name: ")
+img.save(f"{filename}.png")
+
+# Open image automatically
+img.show()
+
+print("QR Code generated successfully!")
+
+# Enter text or URL to generate QR Code: https://www.google.com
+# Enter file name (without extension): google_qr
+
+# QR Code saved successfully as google_qr.png
